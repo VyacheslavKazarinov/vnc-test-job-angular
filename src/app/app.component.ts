80559b784f54c004b0d7f2c45f0b7e9b55a7f8e3
Vyacheslav Kazarinov <vyacheslav.kazarinov@incode-it.com>
2019-10-22 15:32:59 +0300

    feature: add header and getOnlineStatus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import {NetworkStatusService} from './services/network-status.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vnc-test';
+  isUserLoggedIn$: Observable<boolean>;
+
+  constructor(private readonly networkStatusService: NetworkStatusService) {
+    this.isUserLoggedIn$ = this.networkStatusService.getOnlineStatus();
+  }
 }
